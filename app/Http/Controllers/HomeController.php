3bbe58ab6a99ebbf4a3da8af6b79d413a90cb9ff
Tuan Nguyen <nguyen@luxcer.com>
2023-04-19 11:47:55 +0700

    Add tyres to homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tyre;
 
 class HomeController extends Controller
 {
     public function index() {
-        return view('client.index');
+      $new_products = Tyre::select('*')->take(4)->orderBy("id", "desc")->get();
+      $best_products = Tyre::select('*')->take(8)->orderBy("id", "desc")->get();
+      return view('client.index', ['new_products'=> $new_products,'best_products' => $best_products] );
     }
 }
--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -12,21 +12,22 @@
     <div class="new-product">
         <h3 class="text-color">Sản phẩm mới cập nhật</h3>
         <div class="list-new-product row mt-4">
+          @foreach($new_products as $new)
             <div class="col-lg-3">
             <div class="card">
                 <div class="card-body">
-                <h5 class="card-title">AS858</h5>
-                <p class="card-text">LONGHAUL</p>
-                <img class="card-img-top" src="{{ asset('client/assets/img/car.png') }}" alt="Product 1">
+                <h5 class="card-title">{{$new->name}}</h5>
+                <p class="card-text">{{$new->drive->name}}</p>
+                <img class="card-img-top" src="{{asset($new->images[0]->image)}}" alt="{{$new->name}}">
                 <div class="sub-desc row mt-3">
                     <div class="col-lg-4">
-                    <p><i class="fa-regular fa-gas-pump"></i> Xe tải</p>
+                    <p><i class="fa-regular fa-gas-pump"></i> {{$new->model->name}} </p>
                     </div>
                     <div class="col-lg-4">
-                    <p><i class="fa-solid fa-user-group"></i> Trazano</p>
+                    <p><i class="fa-solid fa-user-group"></i> {{$new->brand->name}}</p>
                     </div>
                     <div class="col-lg-4">
-                    <p> Steer/Trailler</p>
+                    <p> {{$new->tyre_structure}}</p>
                     </div>
                 </div>
                 <div class="row mt">
@@ -40,88 +41,7 @@
                 </div>
             </div>
             </div>
-            <div class="col-lg-3">
-            <div class="card">
-                <div class="card-body">
-                <h5 class="card-title">AS858</h5>
-                <p class="card-text">LONGHAUL</p>
-                <img class="card-img-top" src="{{ asset('client/assets/img/car1.png') }}" alt="Product 1">
-                <div class="sub-desc row mt-3">
-                    <div class="col-lg-4">
-                    <p><i class="fa-regular fa-gas-pump"></i> Xe tải</p>
-                    </div>
-                    <div class="col-lg-4">
-                    <p><i class="fa-solid fa-user-group"></i> Trazano</p>
-                    </div>
-                    <div class="col-lg-4">
-                    <p> Steer/Trailler</p>
-                    </div>
-                </div>
-                <div class="row mt">
-                    <div class="col-lg-6">
-                    <p>1.000.000Đ / Lốp 1.500.00Đ</p>
-                    </div>
-                    <div class="col-lg-6 text-center">
-                    <a href="#" class="btn btn-success">Chi tiết</a>
-                    </div>
-                </div>
-                </div>
-            </div>
-            </div> <div class="col-lg-3">
-            <div class="card">
-                <div class="card-body">
-                <h5 class="card-title">AS858</h5>
-                <p class="card-text">LONGHAUL</p>
-                <img class="card-img-top" src="{{ asset('client/assets/img/car.png') }}" alt="Product 1">
-                <div class="sub-desc row mt-3">
-                    <div class="col-lg-4">
-                    <p><i class="fa-regular fa-gas-pump"></i> Xe tải</p>
-                    </div>
-                    <div class="col-lg-4">
-                    <p><i class="fa-solid fa-user-group"></i> Trazano</p>
-                    </div>
-                    <div class="col-lg-4">
-                    <p> Steer/Trailler</p>
-                    </div>
-                </div>
-                <div class="row mt">
-                    <div class="col-lg-6">
-                    <p>1.000.000Đ / Lốp 1.500.00Đ</p>
-                    </div>
-                    <div class="col-lg-6 text-center">
-                    <a href="#" class="btn btn-success">Chi tiết</a>
-                    </div>
-                </div>
-                </div>
-            </div>
-            </div> <div class="col-lg-3">
-            <div class="card">
-                <div class="card-body">
-                <h5 class="card-title">AS858</h5>
-                <p class="card-text">LONGHAUL</p>
-                <img class="card-img-top" src="{{ asset('client/assets/img/car1.png') }}" alt="Product 1">
-                <div class="sub-desc row mt-3">
-                    <div class="col-lg-4">
-                    <p><i class="fa-regular fa-gas-pump"></i> Xe tải</p>
-                    </div>
-                    <div class="col-lg-4">
-                    <p><i class="fa-solid fa-user-group"></i> Trazano</p>
-                    </div>
-                    <div class="col-lg-4">
-                    <p> Steer/Trailler</p>
-                    </div>
-                </div>
-                <div class="row mt">
-                    <div class="col-lg-6">
-                    <p>1.000.000Đ / Lốp 1.500.00Đ</p>
-                    </div>
-                    <div class="col-lg-6 text-center">
-                    <a href="#" class="btn btn-success">Chi tiết</a>
-                    </div>
-                </div>
-                </div>
-            </div>
-            </div>
+          @endforeach
         </div>
     </div>
     <!-- end new product -->
@@ -130,12 +50,13 @@
     <div class="product-best-seller mt-4">
     <h3 class="text-color">Sản phẩm bán chạy</h3>
         <div class="list-new-product row">
+        @foreach($best_products as $best)
         <div class="col-lg-3 mt-4">
             <div class="card">
             <div class="card-body">
-                <h5 class="card-title">AS858</h5>
-                <p class="card-text">LONGHAUL</p>
-                <img class="card-img-top" src="{{ asset('client/assets/img/car.png') }}" alt="Product 1">
+                <h5 class="card-title">{{$best->name}}</h5>
+                <p class="card-text">{{$best->drive->name}}</p>
+                <img class="card-img-top" src="{{asset($best->images[0]->image)}}" alt="{{$best->name}}">
                 <div class="sub-desc row mt-3">
                 <div class="col-lg-4">
                     <p><i class="fa-regular fa-gas-pump"></i> Xe tải</p>
@@ -158,202 +79,7 @@
             </div>
             </div>
         </div>
-        <div class="col-lg-3 mt-4">
-            <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">AS858</h5>
-                <p class="card-text">LONGHAUL</p>
-                <img class="card-img-top" src="{{ asset('client/assets/img/car1.png') }}" alt="Product 1">
-                <div class="sub-desc row mt-3">
-                <div class="col-lg-4">
-                    <p><i class="fa-regular fa-gas-pump"></i> Xe tải</p>
-                </div>
-                <div class="col-lg-4">
-                    <p><i class="fa-solid fa-user-group"></i> Trazano</p>
-                </div>
-                <div class="col-lg-4">
-                    <p> Steer/Trailler</p>
-                </div>
-                </div>
-                <div class="row mt">
-                <div class="col-lg-6">
-                    <p>1.000.000Đ / Lốp 1.500.00Đ</p>
-                </div>
-                <div class="col-lg-6 text-center">
-                    <a href="#" class="btn btn-success">Chi tiết</a>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div> 
-        <div class="col-lg-3 mt-4">
-            <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">AS858</h5>
-                <p class="card-text">LONGHAUL</p>
-                <img class="card-img-top" src="{{ asset('client/assets/img/car.png') }}" alt="Product 1">
-                <div class="sub-desc row mt-3">
-                <div class="col-lg-4">
-                    <p><i class="fa-regular fa-gas-pump"></i> Xe tải</p>
-                </div>
-                <div class="col-lg-4">
-                    <p><i class="fa-solid fa-user-group"></i> Trazano</p>
-                </div>
-                <div class="col-lg-4">
-                    <p> Steer/Trailler</p>
-                </div>
-                </div>
-                <div class="row mt">
-                <div class="col-lg-6">
-                    <p>1.000.000Đ / Lốp 1.500.00Đ</p>
-                </div>
-                <div class="col-lg-6 text-center">
-                    <a href="#" class="btn btn-success">Chi tiết</a>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div> 
-        <div class="col-lg-3 mt-4">
-            <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">AS858</h5>
-                <p class="card-text">LONGHAUL</p>
-                <img class="card-img-top" src="{{ asset('client/assets/img/car1.png') }}" alt="Product 1">
-                <div class="sub-desc row mt-3">
-                <div class="col-lg-4">
-                    <p><i class="fa-regular fa-gas-pump"></i> Xe tải</p>
-                </div>
-                <div class="col-lg-4">
-                    <p><i class="fa-solid fa-user-group"></i> Trazano</p>
-                </div>
-                <div class="col-lg-4">
-                    <p> Steer/Trailler</p>
-                </div>
-                </div>
-                <div class="row mt">
-                <div class="col-lg-6">
-                    <p>1.000.000Đ / Lốp 1.500.00Đ</p>
-                </div>
-                <div class="col-lg-6 text-center">
-                    <a href="#" class="btn btn-success">Chi tiết</a>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div>
-        <div class="col-lg-3 mt-4">
-            <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">AS858</h5>
-                <p class="card-text">LONGHAUL</p>
-                <img class="card-img-top" src="{{ asset('client/assets/img/car.png') }}" alt="Product 1">
-                <div class="sub-desc row mt-3">
-                <div class="col-lg-4">
-                    <p><i class="fa-regular fa-gas-pump"></i> Xe tải</p>
-                </div>
-                <div class="col-lg-4">
-                    <p><i class="fa-solid fa-user-group"></i> Trazano</p>
-                </div>
-                <div class="col-lg-4">
-                    <p> Steer/Trailler</p>
-                </div>
-                </div>
-                <div class="row mt">
-                <div class="col-lg-6">
-                    <p>1.000.000Đ / Lốp 1.500.00Đ</p>
-                </div>
-                <div class="col-lg-6 text-center">
-                    <a href="#" class="btn btn-success">Chi tiết</a>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div>
-        <div class="col-lg-3 mt-4">
-            <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">AS858</h5>
-                <p class="card-text">LONGHAUL</p>
-                <img class="card-img-top" src="{{ asset('client/assets/img/car1.png') }}" alt="Product 1">
-                <div class="sub-desc row mt-3">
-                <div class="col-lg-4">
-                    <p><i class="fa-regular fa-gas-pump"></i> Xe tải</p>
-                </div>
-                <div class="col-lg-4">
-                    <p><i class="fa-solid fa-user-group"></i> Trazano</p>
-                </div>
-                <div class="col-lg-4">
-                    <p> Steer/Trailler</p>
-                </div>
-                </div>
-                <div class="row mt">
-                <div class="col-lg-6">
-                    <p>1.000.000Đ / Lốp 1.500.00Đ</p>
-                </div>
-                <div class="col-lg-6 text-center">
-                    <a href="#" class="btn btn-success">Chi tiết</a>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div> 
-        <div class="col-lg-3 mt-4">
-            <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">AS858</h5>
-                <p class="card-text">LONGHAUL</p>
-                <img class="card-img-top" src="{{ asset('client/assets/img/car.png') }}" alt="Product 1">
-                <div class="sub-desc row mt-3">
-                <div class="col-lg-4">
-                    <p><i class="fa-regular fa-gas-pump"></i> Xe tải</p>
-                </div>
-                <div class="col-lg-4">
-                    <p><i class="fa-solid fa-user-group"></i> Trazano</p>
-                </div>
-                <div class="col-lg-4">
-                    <p> Steer/Trailler</p>
-                </div>
-                </div>
-                <div class="row mt">
-                <div class="col-lg-6">
-                    <p>1.000.000Đ / Lốp 1.500.00Đ</p>
-                </div>
-                <div class="col-lg-6 text-center">
-                    <a href="#" class="btn btn-success">Chi tiết</a>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div> 
-        <div class="col-lg-3 mt-4">
-            <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">AS858</h5>
-                <p class="card-text">LONGHAUL</p>
-                <img class="card-img-top" src="{{ asset('client/assets/img/car1.png') }}" alt="Product 1">
-                <div class="sub-desc row mt-3">
-                <div class="col-lg-4">
-                    <p><i class="fa-regular fa-gas-pump"></i> Xe tải</p>
-                </div>
-                <div class="col-lg-4">
-                    <p><i class="fa-solid fa-user-group"></i> Trazano</p>
-                </div>
-                <div class="col-lg-4">
-                    <p> Steer/Trailler</p>
-                </div>
-                </div>
-                <div class="row mt">
-                <div class="col-lg-6">
-                    <p>1.000.000Đ / Lốp 1.500.00Đ</p>
-                </div>
-                <div class="col-lg-6 text-center">
-                    <a href="#" class="btn btn-success">Chi tiết</a>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div>
+        @endforeach
         </div>
     </div>
 </div>
