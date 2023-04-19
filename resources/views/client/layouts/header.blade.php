c0248f3af05609636a604fb15ac5f1c0c04039a0
Duy Can <duycan@luxcer.com>
2023-04-19 11:18:23 +0700

    layout-client

---
+++ resources/views/client/layouts/header.blade.php
@@ -0,0 +1,45 @@
+<header class="header-site">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light ">
+            <div class="col-lg-2">
+                <a href="#" class="navbar-brand"><img src="{{ asset('client/assets/img/logo.png') }}" alt=""></a>
+            </div>
+            <div class="col-lg-10">
+                <div class="row mt-4">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Sản phẩm <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Dịch vụ</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Tìm đại lý</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Trazano</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Thông tin</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Hỗ trợ</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="row mt-4">
+                    <div class="form-group has-search offset-lg-2">
+                        <span class="form-control-feedback"><i class="fa-light fa-magnifying-glass"></i></span>
+                        <input type="text" class="form-control" placeholder="Tìm kiếm mã gai hoặc size lốp">
+                    </div>
+                </div>
+            </div>
+            </nav>
+    </div>
+</header>
