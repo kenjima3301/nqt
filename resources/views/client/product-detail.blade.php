ecca9a088b3fdb7991ff3116404a4024b2d6b6ea
Tuan Nguyen <nguyen@luxcer.com>
2024-05-16 13:49:25 +0700

    update slide

--- resources/views/client/product-detail.blade.php
+++ resources/views/client/product-detail.blade.php
@@ -598,7 +598,7 @@ span.onclick = function() {
 @endforeach
 </script>
 
-<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<!--<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
 
 <script type="text/javascript">
@@ -612,6 +612,6 @@ span.onclick = function() {
             "autoWidth": false,
         });
     });
-</script>
+</script>-->
 
 @endsection
